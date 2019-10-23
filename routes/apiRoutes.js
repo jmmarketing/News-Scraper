c15108bc246d107746a3362b093193fca95d554a
JeffM <jeff@jeffreymclean.com>
2019-10-23 09:30:55 -0700

    Removed html files after moving to handlebars

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,37 +4,37 @@ var cheerio = require("cheerio");
 
 module.exports = function (app) {
 
-    app.get("/scrape", function (req, res) {
-        console.log("Scrape Started")
-        axios.get("http://blackhatworld.com").then(function (response) {
-            var $ = cheerio.load(response.data);
+    // app.get("/scrape", function (req, res) {
+    //     console.log("Scrape Started")
+    //     axios.get("http://blackhatworld.com").then(function (response) {
+    //         var $ = cheerio.load(response.data);
 
-            $(".discussionListItem visible").each(function (i, element) {
-                var result = {};
+    //         $(".discussionListItem visible").each(function (i, element) {
+    //             var result = {};
 
-                result.title = $(element)
-                    .find("h3 a")
-                    .text();
-                result.link = $(element)
-                    .find("h3 a")
-                    .attr("href");
-                result.views = $(element)
-                    .find("dl .minor")
-                    .children("dd").text()
-                result.replies = $(element)
-                    .find("dl .major")
-                    .children("dd").text()
+    //             result.title = $(element)
+    //                 .find("h3 a")
+    //                 .text();
+    //             result.link = $(element)
+    //                 .find("h3 a")
+    //                 .attr("href");
+    //             result.views = $(element)
+    //                 .find("dl .minor")
+    //                 .children("dd").text()
+    //             result.replies = $(element)
+    //                 .find("dl .major")
+    //                 .children("dd").text()
 
-                db.Article.create(result).then(function (dbArticle) {
-                    console.log(dbArticle);
-                }).catch(function (err) {
-                    console.log(err);
-                });
+    //             db.Article.create(result).then(function (dbArticle) {
+    //                 console.log(dbArticle);
+    //             }).catch(function (err) {
+    //                 console.log(err);
+    //             });
 
-            });
+    //         });
 
-            res.send("Done Scraping");
-        })
-    })
+    //         res.send("Done Scraping");
+    //     })
+    // })
 
 };
